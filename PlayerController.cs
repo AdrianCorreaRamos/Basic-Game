8196af0f92e6608c98f611f08fb29cc780673384
Adrian Correa Ramos <38369249+AdrianCorreaRamos@users.noreply.github.com>
2023-12-19 22:52:52 -0600

    chore: player controll added
    Fixed the player controller function, This commit includes:
    - Rotation based on player keys (left/right)
    - Acceleration based on player keys (up/down)
    - The environment files have been organized
    
    This commit does not introduce new features or bug fixes.

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0.5f;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +19,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on vertical input
         //transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        // Rotates the car based on horizontal input
     }
 }
